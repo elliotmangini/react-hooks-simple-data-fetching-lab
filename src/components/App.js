0361947f8571e131351877c992d4a4ed2899eebd
Elliot Mangini <elliot.mangini@gmail.com>
2022-09-06 11:06:44 -0600

    nice lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+export default function App() {
+    const [ image , setImage ] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(data => setImage(data.message))
+    }, []);
+
+    if (!image) return (
+        <p>Loading...</p>
+    )
+
+    return (
+        <>
+            <img src={image} alt="A Random Dog"></img>
+        </>
+    )
+}
